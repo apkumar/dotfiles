7ea9f75ff4b526039d1898d74e7ffbf15fb15b15
Ankit Kumar <ankit.kumar@discordapp.com>
2022-03-15 19:19:00 +0000

    add install

---
+++ install.sh
@@ -0,0 +1,9 @@
+# Install neovim
+
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+chmod u+x nvim.appimage
+./nvim.appimage --appimage-extract
+sudo mv squashfs-root /
+sudo ln -s /squashfs-root/AppRun /usr/bin/nvim
+
+ln -sf .config/nvim $HOME/.config/nvim
